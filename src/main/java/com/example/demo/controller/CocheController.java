8c0300668ba42ea39ea04bbe40a6fbcf9b326f28
Clauribi <claudiarivasbielsa@gmail.com>
2022-09-20 23:03:40 +0200

    terminando el ejercicio

--- src/main/java/com/example/demo/controller/CocheController.java
+++ src/main/java/com/example/demo/controller/CocheController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.*;
 public class CocheController {
     @GetMapping("/coches/{matricula}/clientes/{dni}")
         public void reservarCoches(@PathVariable String matricula, @PathVariable String dni) {
-            System.out.println("Se reserva el coche con matricula: "+ matricula + "al cliente con dni: "+ dni);
+            System.out.println("Se reserva el coche con matricula: "+ matricula +" " +"al cliente con dni: "+ dni);
         }
-    @DeleteMapping("/coches/{matricula}/clientes/{dni}")
-        public void cancelarReservaCoches(@PathVariable String matricula, @PathVariable String dni) {
-        System.out.println("Se cancela reserva del coche con matricula: "+matricula+"al cliente con dni: "+dni);
+    @DeleteMapping("/coches/{matricula}")
+        public void cancelarReservaCoches(@PathVariable String matricula) {
+        System.out.println("Se cancela reserva del coche con matricula: "+matricula);
     }
 }
 
