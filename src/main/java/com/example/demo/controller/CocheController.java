05494484ebbb6ae92ee360137ae1a1c1f90961ef
Clauribi <claudiarivasbielsa@gmail.com>
2022-09-20 22:56:49 +0200

    reservas

--- src/main/java/com/example/demo/controller/CocheController.java
+++ src/main/java/com/example/demo/controller/CocheController.java
@@ -4,18 +4,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CocheController {
-    @PostMapping("/coches/{matricula}/clientes/{dni}")
-        public void reservarCoches(@PathVariable String matricula, @PathVariable String dni, @RequestBody CocheInput coche) {
+    @GetMapping("/coches/{matricula}/clientes/{dni}")
+        public void reservarCoches(@PathVariable String matricula, @PathVariable String dni) {
             System.out.println("Se reserva el coche con matricula: "+ matricula + "al cliente con dni: "+ dni);
         }
     @DeleteMapping("/coches/{matricula}/clientes/{dni}")
         public void cancelarReservaCoches(@PathVariable String matricula, @PathVariable String dni) {
         System.out.println("Se cancela reserva del coche con matricula: "+matricula+"al cliente con dni: "+dni);
     }
-
-
-
-
-    }
+}
 
 
