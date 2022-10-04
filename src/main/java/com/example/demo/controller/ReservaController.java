e5b6f6d829d6e20d7e87c092ef6394e715744918
Clauribi <claudiarivasbielsa@gmail.com>
2022-10-04 12:45:57 +0200

    fin

--- src/main/java/com/example/demo/controller/ReservaController.java
+++ src/main/java/com/example/demo/controller/ReservaController.java
@@ -1,45 +1,26 @@
 package com.example.demo.controller;
 
-import com.example.demo.domain.Concesionario;
-import com.example.demo.domain.ExisteExcepcion;
 import com.example.demo.domain.Reserva;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.ArrayList;
 
 @RestController
 public class ReservaController {
-    private Concesionario concesionario = new Concesionario();
+    private ArrayList<Reserva> listadoReservas = new ArrayList<>();
 
-    @PostMapping("/reservas")
-    public ResponseEntity<String> altaReservas(@Valid @RequestBody Reserva reserva) {
-        try {
-            concesionario.hacerReserva(reserva);
-        } catch (ExisteExcepcion e) {
-            e.printStackTrace();
-            return ResponseEntity.badRequest().build();
-        }
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
+    @PostMapping("/reservas/coche/{matricula}/cliente/{dni}")
+    public void reservasByMatriculaByDni(@PathVariable String matricula, @PathVariable String dni) {
+        Reserva reserva = new Reserva(matricula, dni);
+        listadoReservas.add(reserva);
 
-    @GetMapping("/reservas/coche/{matricula}/cliente/{dni}")
-    public String reservasByMatriculaByDni(@PathVariable String matricula, @PathVariable String dni) {
-
-        ArrayList<Reserva> listaReservas = new ArrayList<>();
-        for (Reserva reserva : concesionario.getListadoReservas().values()) {
-            if (matricula == reserva.getMatricula() && dni == reserva.getDni()) {
-                listaReservas.add(reserva);
-            }
-        }
-        return listaReservas.toString();
 
     }
-
     @DeleteMapping("/reservas/coche/{matricula}/cliente/{dni}")
-    public void cancelarReservaCoches(@PathVariable String matricula) {
+    public void cancelarReservaCoches(@PathVariable String matricula, @PathVariable String dni) {
+        Reserva reserva = new Reserva(matricula, dni);
+        listadoReservas.remove(reserva);
+
     }
 }
 
